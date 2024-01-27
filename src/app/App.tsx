54719c8a87f387f4dc9b74145922525d77e8dc23
Никита Комаров <nikitakmrv76@gmail.com>
2024-01-27 18:43:17 +0300

    added Header

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,11 @@
-import './App.css'
+import Header from "../components/Header";
 
 function App() {
-
   return (
     <>
+      <Header />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
