9c8725171fe823ee97c671db3bc294ba16bf9a31
Никита Комаров <nikitakmrv76@gmail.com>
2024-01-30 19:09:09 +0300

    added a new error404

--- src/pages/Page404/Page404.tsx
+++ src/pages/Page404/Page404.tsx
@@ -1,10 +1,37 @@
 import Typography from '@mui/material/Typography'
+import Box from '@mui/material/Box'
+import error404_ from '../../shared/assets/error404_.png'
+import Button from '@mui/material/Button'
+import { Link } from 'react-router-dom'
 
 function Page404() {
     return (
-        <Typography variant="h1" gutterBottom>
-            ERROR 404
-        </Typography>
+        <Box
+            style={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+            }}
+        >
+            <Box
+                style={{
+                    backgroundImage: `url(${error404_})`,
+                    backgroundSize: 'contain',
+                    backgroundRepeat: 'no-repeat',
+                    height: '200px',
+                    width: '50%',
+                }}
+            />
+            <Typography variant="h4" gutterBottom>
+                Ничего не найдено, попробуй снова
+            </Typography>
+            <Link to="/">
+                <Button variant="contained" color="success">
+                    Продолжить
+                </Button>
+            </Link>
+        </Box>
     )
 }
 
